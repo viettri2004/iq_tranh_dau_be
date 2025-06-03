d0185ea2c01ab09ebc3c945a099830f005071106
viettri2004 <120078791+viettri2004@users.noreply.github.com>
2025-06-03 22:52:42 +0700

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -37,12 +37,12 @@ import { LeaderboardModule } from './leaderboard/leaderboard.module';
     }),
     TypeOrmModule.forRoot({
       
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
       ssl: {
         rejectUnauthorized: false, 
       },
